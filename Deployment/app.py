cc5f75be65cc249fa3a4516f8b4b9af201c5e8a6
Rishabh Rao <63785806+rishabhrao1997@users.noreply.github.com>
2020-10-30 21:48:38 +0530

    Update app.py

--- Deployment/app.py
+++ Deployment/app.py
@@ -16,6 +16,7 @@ test_predictor_class = final_pipeline(file_directory = file_directory)
 
 with open(file_directory + 'application_table_dtypes.pkl', 'rb') as f:
 	application_dtypes = pickle.load(f)
+	
 #home page
 @app.route('/', methods = ['GET'])
 def home_page():
@@ -30,18 +31,19 @@ def inputs_page():
 @app.route('/predict', methods = ['POST'])
 def prediction():
 	conn = sqlite3.connect(file_directory + 'HOME_CREDIT_DB.db')
-    	#getting the SK_ID_CURR from user
+    #getting the SK_ID_CURR from user
 	sk_id_curr = request.form.to_dict()['SK_ID_CURR']
 	sk_id_curr = int(sk_id_curr)
 	test_datapoint = pd.read_sql_query(f'SELECT * FROM applications WHERE SK_ID_CURR == {sk_id_curr}', conn)
 	test_datapoint = test_datapoint.replace([None], np.nan)
 	test_datapoint = test_datapoint.astype(application_dtypes)
-	predicted_class, data_for_display = test_predictor_class.predict(test_datapoint)
+	predicted_proba, predicted_class, data_for_display = test_predictor_class.predict(test_datapoint)
 
 	if predicted_class == 1:
 		prediction = 'a Potential Defaulter'
 	else:
 		prediction = 'not a Defaulter'
+		predicted_proba = 1 - predicted_proba
 
 	data_for_display = pd.concat([test_datapoint[['NAME_EDUCATION_TYPE', 'OCCUPATION_TYPE']], data_for_display.reset_index(drop = True)], axis = 1)
 	data_for_display = data_for_display.to_html(classes = 'data', header = 'true', index = False)
@@ -49,7 +51,8 @@ def prediction():
 	conn.close()
 	gc.collect()
 
-	return flask.render_template('result_and_inference.html', tables = [data_for_display], output_class = prediction, sk_id_curr = sk_id_curr)
+	return flask.render_template('result_and_inference.html', tables = [data_for_display],
+			output_proba = predicted_proba, output_class = prediction, sk_id_curr = sk_id_curr)
 
 if __name__ == '__main__':
 	app.run(host = '0.0.0.0', port = 5000)
